cb404262134507c5701081c1195783a0ae04627e
dear-mahmud-bd <dearmahmud.bd@gmail.com>
2024-04-09 15:06:26 +0600

    simple project with backend

---
+++ project14_simple_ecommerce/cart/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import cart, add_cart_product, remove_cart_product, remove_cart_item
+
+
+urlpatterns = [
+    path('', cart, name='cart_page'),
+    path('add_cart_product/<int:product_id>/', add_cart_product, name='add_cart_product'),
+    path('remove_cart_product/<int:product_id>/<int:cart_item_id>/', remove_cart_product, name='remove_cart_product'),
+    path('remove_cart_item/<int:product_id>/<int:cart_item_id>/', remove_cart_item, name='remove_cart_item'),
+]
